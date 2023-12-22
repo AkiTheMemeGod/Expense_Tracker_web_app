c9ad91277117e24fee559d3415abfc96dbaa48f4
AkiTheMemeGod <k.akashkumar@gmail.com>
2023-12-22 12:27:55 +0530

    finsihed all the logic for expene and income tracking

--- functions.py
+++ functions.py
@@ -1,13 +1,12 @@
-import time as t
-def get_types(filepath="income_types.txt"):
+import pandas as pd
+def get_types(filepath):
     """This function helps to read the todos you enter into a text file"""
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
 
-
-def put_todos(tds, filepath="income_types.txt"):
+def put_todos(tds, filepath):
     """This function helps to put the todos you enter into a text file"""
     with open(filepath, 'w') as file:
         file.writelines(tds)
@@ -20,5 +19,42 @@ def send_mail(msg):
     s.starttls()
     s.login('akis.pwdchecker@gmail.com', 'tjjqhaifdobuluhg')
     s.sendmail('akis.pwdchecker@gmail.com', 'k.akashkumar@gmail.com', msg=msg)
+
+
+def wrt_csv(date: list, month: list, cat: list, typ: list, amt: list, desc: list):
+
+    data = {
+        "Date": date,
+        "Month": month,
+        "Category": cat,
+        "Type": typ,
+        "Amount": amt,
+        "Description": desc
+    }
+    df = pd.DataFrame(data)
+    df.to_csv("report.csv",index=False,index_label=None)
+
+
+# wrt_csv(["January - 24", "Febrauray - 24"], ["Expense","Income"],["Fuel","Business"],["300","5000"])
+def rd_csv(d: int, m, c, t, a: int, des: str):
+    x = pd.read_csv("report.csv", sep=",")
+
+    date = list(x["Date"])
+    month = list(x["Month"])
+    cat = list(x["Category"])
+    typ = list(x["Type"])
+    amt = list(x["Amount"])
+    desc = list(x["Description"])
+
+    date.append(d)
+    month.append(m)
+    cat.append(c)
+    typ.append(t)
+    amt.append(a)
+    desc.append(des)
+    x.reset_index(drop=True,inplace=True)
+    x.to_csv("report.csv",index=False,index_label=None)
+    wrt_csv(date, month, cat, typ, amt, desc)
+
 
 
--- main.py
+++ main.py
@@ -1,51 +1,110 @@
 import streamlit as st
 from functions import *
 from streamlit_option_menu import option_menu
+import time as t
+st.set_page_config("wide")
+
+def chart():
+    x = pd.read_csv("report.csv", sep=",")
+    date = list(x["Date"])
+    month = list(x["Month"])
+    cat = list(x["Category"])
+    typ = list(x["Type"])
+    amt = list(x["Amount"])
+    desc = list(x["Description"])
+
+    data = {
+        "Date": date,
+        "Month": month,
+        "Category": cat,
+        "Type": typ,
+        "Amount": amt,
+        "Description": desc
+    }
+    return data
+
+
+dat = chart()
+
+
+def report():
+    df = pd.read_csv("report.csv",index_col=False)
+    st.dataframe(df,use_container_width=True)
+    category = st.sidebar.multiselect(label="Select a Category",
+        options=df["Category"].unique(),
+        default=df["Category"].unique(),
+    )
+    st.bar_chart({"Type":dat["Type"]})
 
 
 def expense_tracker():
     st.title("Expense Tracker")
+    cat = "Expense"
 
-    expense = get_types()
+    expense = get_types("expense_types.txt")
 
     types = st.selectbox(label="Select the type of expense:red[*]",
-                         options=expense, key="type")
+                         options=expense, key="type1")
     if types == "Other\n":
         other = st.text_input(label="Enter the type of expense: ")
         if other:
             expense.append(f"{other}\n")
-            put_todos(expense)
-    amount = st.text_input(label="Amount:red[*]", placeholder="Enter the amount")
-    desc = st.text_area(label="Description", placeholder="Tell something about your expense (optional)")
-    st.button("Add Expense", on_click=track(types, amount))
+            put_todos(expense,"expense_types.txt")
+    amount = st.text_input(label="Amount:red[*]", placeholder="Enter the amount",key="amt")
+    desc = st.text_area(label="Description", placeholder="Tell something about your expense (optional)",key="desc")
+    col1, col2 = st.columns(2)
+    with col1:
+        clicked = st.button("Add Expense", on_click=lambda: track(cat, types, amount, desc), key="expense",use_container_width=True)
+
+    with col2:
+        st.button("Reset", on_click=reset_fields,use_container_width=True)
+
 
 def income_tracker():
     st.title("Income Tracker")
-
-    expense = get_types()
+    expense = get_types("income_types.txt")
+    cat = "Income"
 
     types = st.selectbox(label="Select the type of expense:red[*]",
-                         options=expense, key="type")
+                         options=expense, key="type2")
     if types == "Other\n":
         other = st.text_input(label="Enter the type of expense: ")
         if other:
             expense.append(f"{other}\n")
-            put_todos(expense)
+            put_todos(expense, "income_types.txt")
     amount = st.text_input(label="Amount:red[*]", placeholder="Enter the amount")
     desc = st.text_area(label="Description", placeholder="Tell something about your expense (optional)")
-    st.button("Add Expense", on_click=track(types, amount))
 
-def track(type: str, amt: str):
+    col1, col2 = st.columns(2)
+    with col1:
+        clicked = st.button("Add Expense", on_click=lambda: track(cat, types, amount, desc), key="expense",
+                            use_container_width=True)
+        if clicked:st.success("Recorded Successfully")
+
+    with col2:
+        st.button("Reset", on_click=reset_fields, use_container_width=True)
+
+
+def track(cat: str, typ: str, amt: str, desc: str):
     if amt.startswith("0"):
         amt = amt[1:]
-    print(type.strip("\n") + " : " + amt + " ₹")
+    if not all([cat, typ, amt, desc]):
+        st.warning("Please fill in all fields before adding an entry.")
+        return
+    rd_csv(d=int(t.strftime("%d")), m=t.strftime("%B"), c=cat, t=typ, a=int(amt), des=desc)
 
 
-#with st.sidebar:
+def reset_fields():
+    st.session_state["type1"] = None
+    st.session_state["amt"] = None
+    st.session_state["desc"] = None
+
+
 option = option_menu(
     menu_title=None,
     options=["Expense", "Income", "Report"],
     orientation="horizontal",
+    icons=["cash-coin", "cash", "file-earmark-bar-graph-fill"],
     default_index=0
 )
 
@@ -54,5 +113,5 @@ if option == "Expense":
 if option == "Income":
     income_tracker()
 if option == "Report":
-    expense_tracker()
+    report()
 
