69a7fe9d18803ad2590348339955f8b8cb0bfb9d
AkiTheMemeGod <k.akashkumar@gmail.com>
2023-12-28 01:29:37 +0530

    added a custom pdf report generator

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import csv2pdf as cp
+from fpdf import FPDF
 import os
 import streamlit as st
 import time as tt
@@ -75,11 +75,87 @@ def rd_csv(d: int, m, c, t, a: int, des: str):
 
 
 def generate_pdf():
-    cp.convert(source=st.session_state.user_csv, destination=st.session_state.user_pdf,
-               size=11,
-               headersize=30,
-               headerfont=r"dependencies/RobotoMono-Bold.ttf",
-               font=r"dependencies/RobotoMono-BoldItalic.ttf")
+    df = pd.read_csv(filepath_or_buffer=st.session_state.user_csv, sep=",", index_col=None)
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    pdf.set_font(family="Times", size=40, style="B")
+    pdf.image(name="Untitled design.png", type="png", w=30, h=30, link="https://spend-it-akash.streamlit.app")
+    pdf.cell(w=20, h=10, txt='', align="C", ln=1)
+    pdf.cell(w=20, h=10, txt='', align="C", ln=1)
+    pdf.cell(w=20, h=10, txt='', align="C", ln=1)
+    pdf.cell(w=20, h=10, txt='', align="C", ln=1)
+
+    pdf.set_text_color(243, 18, 18)
+    pdf.text(txt="$pend-It.", x=75, y=20)
+
+    pdf.set_text_color(100, 100, 100)
+    pdf.set_font(family="Times", size=20, style="B")
+    pdf.text(txt="Expense Report", x=77, y=35)
+
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_font(family="Times", size=18, style="B")
+    pdf.text(txt="AkiTheMemeGod", x=156, y=25)
+
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_font(family="Times", size=18, style="B")
+    pdf.text(txt=tt.strftime("Date : %d/%m/%y"), x=165, y=35)
+    pdf.line(x1=10, x2=200, y1=45, y2=45)
+
+    pdf.set_font(family="times", size=25, style="B")
+    pdf.set_text_color(50, 50, 50)
+    pdf.cell(w=20, h=10, txt='Date', align="C")
+    pdf.cell(w=50, h=10, txt='Month', align="C")
+    pdf.cell(w=40, h=10, txt='Category', align="C")
+    pdf.cell(w=55, h=10, txt='Type', align="C")
+    pdf.cell(w=32, h=10, txt='Amount', ln=1, align="C")
+    pdf.cell(w=20, h=3, txt='', align="C")
+    pdf.cell(w=50, h=3, txt='', align="C")
+    pdf.cell(w=40, h=3, txt='', align="C")
+    pdf.cell(w=55, h=3, txt='', align="C")
+    pdf.cell(w=32, h=3, txt='', ln=1, align="C")
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=25)
+        pdf.set_text_color(50, 50, 50)
+        pdf.cell(w=20, h=10, txt=str(row['Date']), border=1, align="C")
+
+        pdf.cell(w=50, h=10, txt=str(row['Month']), border=1, align="C")
+        pdf.cell(w=40, h=10, txt=str(row['Category']), border=1, align="C")
+        pdf.cell(w=55, h=10, txt=str(row['Type']), border=1, align="C")
+        pdf.cell(w=32, h=10, txt=str(row['Amount']), border=1, ln=1, align="C")
+
+    total_income = str(df.groupby(by=['Category']).sum()[['Amount']]['Amount'][1])
+    total_expense = str(df.groupby(by=['Category']).sum()[['Amount']]['Amount'][0])
+
+    pdf.set_font(family="Times", size=25, style="B")
+    pdf.cell(w=20, h=3, txt='', align="C")
+    pdf.cell(w=50, h=3, txt='', align="C")
+    pdf.cell(w=40, h=3, txt='', align="C")
+    pdf.cell(w=55, h=3, txt='', align="C")
+    pdf.cell(w=32, h=3, txt='', ln=1, align="C")
+    pdf.set_text_color(33, 179, 42)
+    pdf.cell(w=20, h=10, txt='', align="C")
+    pdf.cell(w=50, h=10, txt='', align="C")
+    pdf.cell(w=40, h=10, txt='Total', align="R")
+    pdf.cell(w=55, h=10, txt='Income : ', align="L")
+    pdf.cell(w=32, h=10, txt=total_income, ln=1, align="L")
+    pdf.set_font(family="Times", size=25, style="B")
+
+    pdf.cell(w=20, h=3, txt='', align="C")
+    pdf.cell(w=50, h=3, txt='', align="C")
+    pdf.cell(w=40, h=3, txt='', align="C")
+    pdf.cell(w=55, h=3, txt='', align="C")
+    pdf.cell(w=32, h=3, txt='', ln=1, align="C")
+    pdf.set_text_color(255, 0, 0)
+    pdf.cell(w=20, h=10, txt='', align="C")
+    pdf.cell(w=50, h=10, txt='', align="C")
+    pdf.cell(w=40, h=10, txt='Total', align="R")
+    pdf.cell(w=55, h=10, txt='Expense : ', align="L")
+    pdf.cell(w=32, h=10, txt=total_expense, ln=1, align="L")
+
+    pdf.output("report.pdf")
 
     with open(st.session_state.user_pdf, "rb") as pdf_file:
         PDFbyte = pdf_file.read()
